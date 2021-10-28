62e308636331c17ca64578e863e6ab7e130abc5b
vionarachma <92927492+vionarachma@users.noreply.github.com>
2021-10-28 22:07:36 +0700

    Add files via upload

---
+++ latihan.py.py
@@ -0,0 +1,5 @@
+print("Assalamu'alaikum")
+print(" ")
+print("Biarkan saya memperkenalkan diri saya.Haii Namaku Viona Rachma Haji Sahbrina..aku lahir di Kota Pemalang tanggal 05 april kelahiran 2001, kalian bisa panngil aku Viona atau Vio. Saya seorang Mahasiswa di Universitas Pelita Bangsa di Prodi Teknik Informatika dengan NIM 312110373 di Kelas T.21.C.3.Rumahku diBumi pastinya hehe di perum.graha Harapan Blok B3/12 Mustika Jaya Bekasi")
+print(" ")
+cetak("Sekian perkenalan singkatku,Terima Kasih")
