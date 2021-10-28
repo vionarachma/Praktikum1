cae4cef2ee61090e7a27084ef9dde04e76791bca
vionarachma <92927492+vionarachma@users.noreply.github.com>
2021-10-28 22:44:41 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,20 @@
+print("hallo")
+print("saya sedang belajar python")
+
+a = 8
+b = 6
+print("variabel a=",a)
+print("variabel b=",b)
+print("hasil penjumlahan a+b",a=b)
+
+a=int(input("masukan nilai a:"))
+b=int(input("masukan nilai b:"))
+print("variabel a=",a)
+print("variabel b=",b)
+print("hasil penggabungan {1}&{0}=%d". format(a,b) %(a+b))
+
+#konversi nilai variabel
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {1}+{0}=%d". format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}%f" . format (a,b) %(a/b))
