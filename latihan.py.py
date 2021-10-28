3176d6ed65813a3639a42c904be5cf250db98f22
vionarachma <92927492+vionarachma@users.noreply.github.com>
2021-10-28 22:25:28 +0700

    Update latihan.py.py

--- latihan.py.py
+++ latihan.py.py
@@ -3,3 +3,13 @@ print(" ")
 print("Biarkan saya memperkenalkan diri saya.Haii Namaku Viona Rachma Haji Sahbrina..aku lahir di Kota Pemalang tanggal 05 april kelahiran 2001, kalian bisa panngil aku Viona atau Vio. Saya seorang Mahasiswa di Universitas Pelita Bangsa di Prodi Teknik Informatika dengan NIM 312110373 di Kelas T.21.C.3.Rumahku diBumi pastinya hehe di perum.graha Harapan Blok B3/12 Mustika Jaya Bekasi")
 print(" ")
 cetak("Sekian perkenalan singkatku,Terima Kasih")
+
+Penjelasan program :
+ 1. perintah print("Assalamu'alaikum) akan menghasilkan output Assalamualikum
+ 2. perintah print (" biarkan Biarkan saya memperkenalkan diri saya.Haii Namaku Viona Rachma Haji Sahbrina..aku lahir di Kota Pemalang tanggal 05 april kelahiran 2001, kalian bisa panngil aku Viona atau Vio. Saya seorang Mahasiswa di Universitas Pelita Bangsa di Prodi Teknik Informatika dengan NIM 312110373 di Kelas T.21.C.3.Rumahku diBumi pastinya hehe di perum.graha Harapan Blok B3/12 Mustika Jaya Bekasi")
+ akan menghasilkan output seperti contoh digambar.
+ 3. perintah cetak("sekian perkenalan singkatku, Terima kasih")
+                   
+                   
+# Berikut adalah gambar dari screnshoot contoh hasil dari ptyhon di atas
+                   
